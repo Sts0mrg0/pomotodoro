7cab82f505042dac244701941fc127e97f8cbb76
Stefan Le Noach <stefanlenoach@gmail.com>
2016-07-15 16:56:53 -0400

    added models

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,9 @@
 class Todo < ActiveRecord::Base
+  validates :title, :url, :user, presence: true
+
+  belongs_to :user,
+    class_name: "User",
+    foreign_key: :user_id,
+    primary_key: :id
+  
 end
