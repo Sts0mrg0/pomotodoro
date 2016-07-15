39d832baa583701bd85926a650a4d9da1186fca6
Stefan Le Noach <stefanlenoach@gmail.com>
2016-07-15 16:48:40 -0400

    db set

--- db/migrate/20160715204210_create_todos.rb
+++ db/migrate/20160715204210_create_todos.rb
@@ -1,8 +1,12 @@
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
-
+      t.string :title, null: false
+      t.string :body
+      t.integer :pomodoros, default: 0
+      t.integer :user_id, null: false
       t.timestamps null: false
     end
+    add_index :todos, :user_id
   end
 end
